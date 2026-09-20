d3b5c251bb9715f7b5817bfcf54de2a44b680816
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-09-20 22:17:33 +0000

    Review of 3rd week Sep

--- neetcode-roadmap/110.py
+++ neetcode-roadmap/110.py
@@ -11,21 +11,20 @@ class Solution:
         ret_flag = True
 
         def check_depth(node):
-            nonlocal ret_flag
-
             if not node:
                 return 0
 
             left_depth = check_depth(node.left)
+            if left_depth == -1:
+                return -1
+            
             right_depth = check_depth(node.right)
-            if not ret_flag:
-                return
+            if right_depth == -1:
+                return -1
 
-            if left_depth - right_depth > 1 or left_depth - right_depth < -1:
-                ret_flag = False
+            if abs(left_depth - right_depth) > 1:
+                return -1
 
             return max(left_depth, right_depth) + 1
 
-        check_depth(root)
-        return ret_flag
-
+        return check_depth(root) != -1
--- neetcode-roadmap/572.py
+++ neetcode-roadmap/572.py
@@ -10,30 +10,18 @@ class Solution:
         def is_same(p, q):
             if not (p or q):
                 return True
-            if not (p and q):
-                return False
 
-            if p.val != q.val:
+            if not p or not q or p.val != q.val:
                 return False
-
-            if not (p.left or p.right or q.left or q.right):
-                return p.val == q.val
 
             return is_same(p.left, q.left) and is_same(p.right, q.right)
 
-        ret_flag = False
-        def check_tree(node):
-            nonlocal ret_flag
-            if ret_flag:
-                return
-            if is_same(node, subRoot):
-                ret_flag = True
-                return
-            if node.left:
-                check_tree(node.left)
-            if node.right:
-                check_tree(node.right)
+        
+        if not root:
+            return False
 
-        check_tree(root)
-        return ret_flag
+        if is_same(root, subRoot):
+            return True
+
+        return self.isSubtree(root=root.left, subRoot=subRoot) or self.isSubtree(root=root.right, subRoot=subRoot)
 
